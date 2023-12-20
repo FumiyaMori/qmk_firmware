a4dbd9dde0ed469880af1aba73754f8970b82416
FumiyaMori <fufufu8m@gmail.com>
2023-12-20 23:52:36 +0900

    add encoder

--- keyboards/sample2x2/keymaps/default/keymap.c
+++ keyboards/sample2x2/keymaps/default/keymap.c
@@ -16,3 +16,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_C,   KC_D
     )
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =   { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT) }
+};
+#endif
